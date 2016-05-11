26e04f4fb32b359ee8074be41c5b99dc92c80f0c
Mark McGuire <mark.b.mcg@gmail.com>
2016-05-10 21:13:35 -0500

    pull mumble-server.sqlite from s3

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+/usr/bin/aws s3 cp ${S3_PATH:?}/mumble-server.sqlite /data/mumble-server.sqlite
 /usr/bin/aws ec2 associate-address --instance-id `curl -s -m 10 http://169.254.169.254/latest/meta-data/instance-id` --allocation-id ${ALLOC_ID:?} --region us-east-1
-/usr/sbin/murmurd -fg -ini /config/mumble-server.ini
+/usr/sbin/murmurd -fg -ini /config/mumble-server.ini -supw ${MURMUR_PW:?}
